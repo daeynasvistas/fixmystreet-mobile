352e83c19be7edf151639bcdcd12ba69eef22ea5
daeynasvistas <netrapida@live.com.pt>
2016-02-11 14:45:44 +0000

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
     // Language of templates to use ( should be name of directory under src/templates/ )
-    LANGUAGE: '',
+    LANGUAGE: 'en',
 
     // Name of app to use in alert dialog titles amongst other things
     APP_NAME: 'Distrito.pt',
